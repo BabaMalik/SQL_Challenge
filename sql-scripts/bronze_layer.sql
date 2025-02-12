3c6cc37a995cb4885647cb1e6f4c566f71d9af27
SHAIK BABA MALIK HUSSAIN <44469525+BabaMalik@users.noreply.github.com>
2025-02-12 22:46:11 +0530

    Update bronze_layer.sql

--- sql-scripts/bronze_layer.sql
+++ sql-scripts/bronze_layer.sql
@@ -1,1 +1,22 @@
 
+CREATE TABLE bronze_transactions (
+    TransactionID INT PRIMARY KEY,
+    CustomerID INT,
+    TransactionDate DATE,
+    TransactionAmount DECIMAL(10,2),
+    PaymentMethod VARCHAR(50),
+    Quantity INT,
+    DiscountPercent DECIMAL(5,2),
+    City VARCHAR(100),
+    StoreType VARCHAR(50),
+    CustomerAge INT,
+    CustomerGender VARCHAR(10),
+    LoyaltyPoints INT,
+    ProductName VARCHAR(100),
+    Region VARCHAR(100),
+    Returned VARCHAR(10),
+    FeedbackScore INT,
+    ShippingCost DECIMAL(10,2),
+    DeliveryTimeDays INT,
+    IsPromotional BOOLEAN
+);
